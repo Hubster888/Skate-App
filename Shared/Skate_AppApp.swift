6cdc71854b79c5a164009cc95064e02c91fe177f
Hubert Rzeminski <hubertrzeminski16@gmail.com>
2020-12-09 13:34:10 +0000

    Filling GitHub.

---
+++ Shared/Skate_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Skate_AppApp.swift
+//  Shared
+//
+//  Created by Hubert Rzeminski on 09/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct Skate_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
